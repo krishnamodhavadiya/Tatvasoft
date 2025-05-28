83f1056274a204acf66296fb95b60a73cb19ea62
krishnamodhavadiya <krishamodha2004@gmail.com>
2025-05-29 00:07:35 +0530

    Add files via upload

---
+++ DAY3/BooksApi/Controllers/BookController.cs
@@ -0,0 +1,81 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BooksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly BookService bookService;
+
+        public BookController(BookService book) 
+        {
+            this.bookService = book;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public ActionResult AddBook(Book book)
+        {
+            this.bookService.AddBook(book);
+            return Ok("Book created !");
+
+        }
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult GetAll()
+        {
+            return Ok(this.bookService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public ActionResult GetById(int id)
+        {
+            var res = this.bookService.GetBookById(id);
+
+            if (res != null) { return Ok(res); }
+
+            return NotFound("Book not found!");
+        }
+
+        // To Update Book
+
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult UpdateBook([FromBody] Book updatedBook)
+        {
+            var existingBook = this.bookService.GetBookById(updatedBook.Id);
+
+            if (existingBook == null)
+            {
+                return NotFound("Book not found!");
+            }
+
+            this.bookService.UpdateBook(updatedBook);
+            return Ok("Book updated successfully!");
+        }
+        // To Delete Book
+
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult DeleteBook(int id)
+        {
+            var existingBook = this.bookService.GetBookById(id);
+
+            if (existingBook == null)
+            {
+                return NotFound("Book not found!");
+            }
+
+            this.bookService.DeleteBook(id);
+            return Ok("Book deleted successfully!");
+        }
+    }
+}
---
+++ DAY3/BooksApi/Services/BookServices.cs
@@ -0,0 +1,54 @@
+﻿using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+    // For CRUD on books
+    public class BookService
+    {
+        private List<Book> _books;
+
+        public BookService()
+        {
+            _books = new List<Book>();
+        }
+
+        // To Add Book
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        // To Get All Books
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        // To Get Single Book
+        public Book? GetBookById(int id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+
+        // To Update Book
+        public void UpdateBook(Book updatedBook)
+        {
+            var book = _books.Find(x => x.Id == updatedBook.Id);
+            if (book != null)
+            {
+                book.Title = updatedBook.Title;
+                book.Author = updatedBook.Author;
+               
+            }
+        }
+        // To Delete Book
+        public void DeleteBook(int id)
+        {
+            var book = _books.Find(x => x.Id == id);
+            if (book != null)
+            {
+                _books.Remove(book);
+            }
+        }
+    }
+}
